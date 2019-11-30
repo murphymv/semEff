23e83790d4183ef1e46f4753d7b575dc58f3efd5
Mark <murphymv@gmail.com>
2019-11-30 18:44:01 +0000

    Update to stdCoeff

--- R/std_coeff_fun.R
+++ R/std_coeff_fun.R
@@ -1135,7 +1135,7 @@ stdCoeff <- function(mod, weights = NULL, data = NULL, term.names = NULL,
     if (std.y) b <- b / sdW(getY(m, link = TRUE), w)
 
     ## Return standardised coefficients
-    xn2 <- xNam(m, envir = environment())
+    xn2 <- xNam(m, envir = d)
     sapply(xn2, function(i) unname(b[i]))
 
   }
